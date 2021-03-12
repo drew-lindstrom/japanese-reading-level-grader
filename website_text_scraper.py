c67c87ed5cd17991e17bc3c180126f60f733ddb8
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-03-12 16:54:41 -0600

    Added parse_text function to parse HTML for unique kanji and total kanji.

--- website_text_scraper.py
+++ website_text_scraper.py
@@ -4,32 +4,45 @@ import requests
 
 
 N5 = 0
+N5_unique = set()
 N4 = 0
+N4_unique = set()
 N3 = 0
+N3_unique = set()
 N2 = 0
+N2_unique = set()
 N1 = 0
+N1_unique = set()
 
 url = "https://www3.nhk.or.jp/nhkworld/ja/"
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 
-for character in soup.text:
-    try:
-        level = kanji_dict[character]
-    except:
-        continue
 
-    if level == "JLPT N5":
-        N5 += 1
-    if level == "JLPT N4":
-        N4 += 1
-    if level == "JLPT N3":
-        N3 += 1
-    if level == "JLPT N2":
-        N2 += 1
-    if level == "JLPT N1":
-        N1 += 1
+def parse_text(soup):
+    for character in soup.text:
+        try:
+            level = kanji_dict[character]
+        except:
+            continue
+
+        if level == "JLPT N5":
+            N5 += 1
+            N5_unique.add(character)
+        if level == "JLPT N4":
+            N4 += 1
+            N4_unique.add(character)
+        if level == "JLPT N3":
+            N3 += 1
+            N3_unique.add(character)
+        if level == "JLPT N2":
+            N2 += 1
+            N2_unique.add(character)
+        if level == "JLPT N1":
+            N1 += 1
+            N1_unique.add(character)
+
 
 print(f"Number of JLPT N5 Characters: {N5}")
 print(f"Number of JLPT N4 Characters: {N4}")
