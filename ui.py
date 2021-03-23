129a6c51efc364627906d541dbf902a575e4880e
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-03-23 15:09:51 -0500

    Added get_total and get_unique_total functions. Print functions now also print percentages.

--- ui.py
+++ ui.py
@@ -1,31 +1,65 @@
 from website_text_scraper import parse_text, get_kanji
 
 
-def print_count(kanji_count):
-    print(f"Number of JLPT N5 Characters: {kanji_count[0]}")
-    print(f"Number of JLPT N4 Characters: {kanji_count[1]}")
-    print(f"Number of JLPT N3 Characters: {kanji_count[2]}")
-    print(f"Number of JLPT N2 Characters: {kanji_count[3]}")
-    print(f"Number of JLPT N1 Characters: {kanji_count[4]}")
+def print_count(kanji_count, total):
+    print(f"Total Number of Characters: {total}")
+    print(f"N5: {kanji_count[0]} ({round((kanji_count[0]/total), 4)*100}%)")
+    print(f"N4: {kanji_count[1]} ({round((kanji_count[1]/total), 4)*100}%)")
+    print(f"N3: {kanji_count[2]} ({round((kanji_count[2]/total), 4)*100}%)")
+    print(f"N2: {kanji_count[3]} ({round((kanji_count[3]/total), 4)*100}%)")
+    print(f"N1: {kanji_count[4]} ({round((kanji_count[4]/total), 4)*100}%)")
     print()
 
 
-def print_count_unique(unique_kanji):
-    print(f"Number of Unique JLPT N5 Characters: {len(unique_kanji['N5'])}")
-    print(f"Number of Unique JLPT N4 Characters: {len(unique_kanji['N4'])}")
-    print(f"Number of Unique JLPT N3 Characters: {len(unique_kanji['N3'])}")
-    print(f"Number of Unique JLPT N2 Characters: {len(unique_kanji['N2'])}")
-    print(f"Number of Unique JLPT N1 Characters: {len(unique_kanji['N1'])}")
+def print_count_unique(unique_kanji, unique_total):
+    print(f"Total Number of Unique Characters: {unique_total}")
+    print(
+        f"N5: {len(unique_kanji['N5'])} ({round((len(unique_kanji['N5'])/unique_total), 4)*100}%)"
+    )
+    print(
+        f"N4: {len(unique_kanji['N4'])} ({round((len(unique_kanji['N4'])/unique_total), 4)*100}%)"
+    )
+    print(
+        f"N3: {len(unique_kanji['N3'])} ({round((len(unique_kanji['N3'])/unique_total), 4)*100}%)"
+    )
+    print(
+        f"N2: {len(unique_kanji['N2'])} ({round((len(unique_kanji['N2'])/unique_total), 4)*100}%)"
+    )
+    print(
+        f"N1: {len(unique_kanji['N1'])} ({round((len(unique_kanji['N1'])/unique_total), 4)*100}%)"
+    )
     print()
+
+
+def get_total(kanji_count):
+    total = 0
+
+    for count in kanji_count:
+        total += count
+
+    return total
+
+
+def get_unique_total(unique_kanji):
+    unique_total = 0
+
+    for key in unique_kanji:
+        unique_total += len(unique_kanji[key])
+
+    return unique_total
 
 
 unique_kanji = {"N5": set(), "N4": set(), "N3": set(), "N2": set(), "N1": set()}
 kanji_count = [0, 0, 0, 0, 0]
+
+# TODO: Error handling for invalid URL
 
 print("Enter URL: ")
 url = input()
 print()
 
 get_kanji(url, kanji_count, unique_kanji)
-print_count(kanji_count)
-print_count_unique(unique_kanji)
+total = get_total(kanji_count)
+unique_total = get_unique_total(unique_kanji)
+print_count(kanji_count, total)
+print_count_unique(unique_kanji, unique_total)
