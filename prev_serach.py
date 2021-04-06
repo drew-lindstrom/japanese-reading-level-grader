118fbcbdadbf49aa87f63443e91e40f8850c227b
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-05 20:14:42 -0500

    Updated to either update or add new entry to prev_search.db

--- prev_serach.py
+++ prev_serach.py
@@ -6,16 +6,18 @@ def update_prev_search_db(url, kanji_count):
     mycursor = db.cursor()
 
     i = (
+        url,
         kanji_count[0],
         kanji_count[1],
         kanji_count[2],
         kanji_count[3],
         kanji_count[4],
-        url,
     )
-
-    mycursor.execute(
-        "UPDATE prev_search SET JLPT_5 = (?), JLPT_4 = (?), JLPT_3 = (?), JLPT_2 = (?), JLPT_1 = (?) WHERE url = (?)",
-        i,
-    )
+    try:
+        mycursor.execute("INSERT INTO prev_search VALUES (?, ?, ?, ?, ?, ?)", i)
+    except Exception:
+        mycursor.execute(
+            "UPDATE prev_search SET JLPT_5 = (?), JLPT_4 = (?), JLPT_3 = (?), JLPT_2 = (?), JLPT_1 = (?) WHERE url = (?)",
+            i,
+        )
     db.commit()
