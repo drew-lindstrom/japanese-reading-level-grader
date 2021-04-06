acd81532dfa241ca032ac9ca195793c725565499
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-06 11:25:36 -0500

    Updated to only add kanji in kanji database to known_kanji set

--- known_kanji.py
+++ known_kanji.py
@@ -1,9 +1,20 @@
+import sqlite3
+
+
 def get_known_kanji():
+    known_kanji = set()
     f = open("Known_Kanji.txt", "r")
 
-    known_kanji = set()
+    db = sqlite3.connect("kanji.db")
+    mycursor = db.cursor()
 
-    for character in f:
-        known_kanji.add(character)
+    for line in f:
+        for char in line:
+            mycursor.execute("SELECT * FROM kanji_table WHERE kanji=?", char)
+            result = mycursor.fetchone()
+            if result:
+                known_kanji.add(result[0])
+
+    f.close()
 
     return known_kanji
