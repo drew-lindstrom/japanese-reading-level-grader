82f0b11f09a7924d0f5c6c4c92ad8815e5ba0ef6
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-05 19:57:15 -0500

    Updated update_prev_search_db function to update already existing entries.

--- prev_serach.py
+++ prev_serach.py
@@ -1,20 +1,21 @@
 import sqlite3
 
-# TODO: Duplicate urls?
-
 
 def update_prev_search_db(url, kanji_count):
     db = sqlite3.connect("kanji.db")
     mycursor = db.cursor()
 
     i = (
-        url,
         kanji_count[0],
         kanji_count[1],
         kanji_count[2],
         kanji_count[3],
         kanji_count[4],
+        url,
     )
 
-    mycursor.execute("INSERT INTO prev_search VALUES (?, ?, ?, ?, ?, ?)", i)
+    mycursor.execute(
+        "UPDATE prev_search SET JLPT_5 = (?), JLPT_4 = (?), JLPT_3 = (?), JLPT_2 = (?), JLPT_1 = (?) WHERE url = (?)",
+        i,
+    )
     db.commit()
