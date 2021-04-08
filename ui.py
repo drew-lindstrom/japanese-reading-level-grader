9b09dd6058b3e152f75c162226cc58c55f586542
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-08 11:54:02 -0500

    Added print_unknown_kanji function.

--- ui.py
+++ ui.py
@@ -33,21 +33,21 @@ def print_unique_count(unique_kanji, unique_total):
     print()
 
 
-def print_reading_ability(kanji_count, total, known_kanji_count):
+def print_reading_ability(kanji_count, known_kanji_count):
     print(
-        f"N5: {known_kanji_count[0]} out of {kanji_count[0]} known. ({round((known_kanji_count[0]/kanji_count[0]), 4)* 100}%"
+        f"N5: {known_kanji_count[0]} out of {kanji_count[0]} known. ({round((known_kanji_count[0]/kanji_count[0]), 4)* 100}%)"
     )
     print(
-        f"N5: {known_kanji_count[1]} out of {kanji_count[1]} known. ({round((known_kanji_count[1]/kanji_count[1]), 4)* 100}%"
+        f"N5: {known_kanji_count[1]} out of {kanji_count[1]} known. ({round((known_kanji_count[1]/kanji_count[1]), 4)* 100}%)"
     )
     print(
-        f"N5: {known_kanji_count[2]} out of {kanji_count[2]} known. ({round((known_kanji_count[2]/kanji_count[2]), 4)* 100}%"
+        f"N5: {known_kanji_count[2]} out of {kanji_count[2]} known. ({round((known_kanji_count[2]/kanji_count[2]), 4)* 100}%)"
     )
     print(
-        f"N5: {known_kanji_count[3]} out of {kanji_count[3]} known. ({round((known_kanji_count[3]/kanji_count[3]), 4)* 100}%"
+        f"N5: {known_kanji_count[3]} out of {kanji_count[3]} known. ({round((known_kanji_count[3]/kanji_count[3]), 4)* 100}%)"
     )
     print(
-        f"N5: {known_kanji_count[4]} out of {kanji_count[4]} known. ({round((known_kanji_count[4]/kanji_count[4]), 4)* 100}%"
+        f"N5: {known_kanji_count[4]} out of {kanji_count[4]} known. ({round((known_kanji_count[4]/kanji_count[4]), 4)* 100}%)"
     )
 
 
@@ -62,6 +62,14 @@ def print_all_time_total(all_time_total):
     print(f"N3: {all_time_total[0][2]}")
     print(f"N2: {all_time_total[0][3]}")
     print(f"N1: {all_time_total[0][4]}")
+    print()
+
+
+def print_unknown_kanji(unknown_kanji):
+    print("Unknown Kanji:")
+    print()
+    for kanji in unknown_kanji:
+        print(kanji)
     print()
 
 
@@ -96,13 +104,17 @@ def reset_kanji_count(kanji_count):
         kanji_count[level] = 0
 
 
-unique_kanji = {"N5": set(), "N4": set(), "N3": set(), "N2": set(), "N1": set()}
-kanji_count = [0, 0, 0, 0, 0]
-
 # TODO: Rounding error
+# TODO: Updated unknown kanji be JLPT level.
 
 
 def main():
+    unique_kanji = {"N5": set(), "N4": set(), "N3": set(), "N2": set(), "N1": set()}
+    kanji_count = [0, 0, 0, 0, 0]
+    known_kanji_count = [0, 0, 0, 0, 0]
+    known_kanji = get_known_kanji()
+    unknown_kanji = set()
+
     while True:
         while True:
             print("Enter URL: ")
@@ -112,7 +124,14 @@ def main():
                 reset_unique_kanji(unique_kanji)
                 reset_kanji_count(kanji_count)
 
-                get_kanji(url, kanji_count, unique_kanji)
+                get_kanji(
+                    url,
+                    kanji_count,
+                    unique_kanji,
+                    known_kanji,
+                    known_kanji_count,
+                    unknown_kanji,
+                )
                 if (
                     kanji_count[0] == 0
                     and kanji_count[1] == 0
@@ -145,9 +164,9 @@ def main():
             print()
 
             if i == "1":
-                pass
+                print_reading_ability(kanji_count, known_kanji_count)
             if i == "2":
-                pass
+                print_unknown_kanji(unknown_kanji)
             if i == "3":
                 all_time_total = get_all_time_total()
                 print_all_time_total(all_time_total)
