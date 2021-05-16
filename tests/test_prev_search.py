674a2f7bbf7ce130dd3b573f35326568a4cf95c5
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-05-16 18:26:04 -0500

    Implemented test_update_prev_search_table.

--- tests/test_prev_search.py
+++ tests/test_prev_search.py
@@ -6,50 +6,38 @@ import pytest
 class TestPrevSearch:
     @pytest.fixture
     def connect_to_temp_prev_search_table(self):
+        """Pytest fixture to connect to database."""
         db = sqlite3.connect("kanji.db")
         return db
 
     @pytest.fixture
     def create_temp_prev_search_table(self, connect_to_temp_prev_search_table):
+        """Pytest fixture to create a temporary prev_search table."""
         cursor = connect_to_temp_prev_search_table
-        while True:
-            try:
-                cursor.execute(
-                    "CREATE TABLE temp_prev_search (url text UNIQUE, JLPT_5 int, JLPT_4 int, JLPT_3 int, JLPT_2 int, JLPT_1 int)"
-                )
-                return cursor
-            except:
-                # delete_temp_prev_search_table
-                return cursor
+        try:
+            self.delete_temp_prev_search_table(cursor)
+        except:
+            pass
+        cursor.execute(
+            "CREATE TABLE temp_prev_search (url text UNIQUE, JLPT_5 int, JLPT_4 int, JLPT_3 int, JLPT_2 int, JLPT_1 int)"
+        )
+        return cursor
 
-    # @pytest.fixture
-    # def delete_temp_prev_search_table(self, connect_to_temp_prev_search_table):
-    #     cursor = connect_to_temp_prev_search_table
-    #     cursor.execute("DROP TABLE temp_prev_search")
+    def delete_temp_prev_search_table(self, cursor):
+        """Deletes temp_prev_search table."""
+        cursor.execute("DROP TABLE temp_prev_search")
 
-    @pytest.mark.parametrize(
-        "url,level,result",
-        [
-            ("testURL", 1, 5),
-            ("testURL", 2, 5),
-            ("testURL", 3, 5),
-            ("testURL", 4, 5),
-            ("testURL", 5, 5),
-        ],
-    )
     def test_update_prev_search_table(
+        self,
         connect_to_temp_prev_search_table,
         create_temp_prev_search_table,
-        url,
-        level,
-        result,
     ):
-        cursor = create_temp_prev_search_table
+        """Tests that update_prev_search_table operates correctly and checks that duplicates aren't present."""
+        cursor = connect_to_temp_prev_search_table
         update_prev_search_table("testURL", [5, 5, 5, 5, 5], "temp_prev_search")
         update_prev_search_table("testURL", [5, 5, 5, 5, 5], "temp_prev_search")
-        assert (
-            cursor.execute(
-                "SELECT * FROM temp_prev_search WHERE url = (?)", url
-            ).fetchone()[level]
-            == result
-        )
+        assert cursor.execute(
+            "SELECT * FROM temp_prev_search WHERE url = (?)", ("testURL",)
+        ).fetchone() == ("testURL", 5, 5, 5, 5, 5)
+        assert len(list(cursor.execute("SELECT * FROM temp_prev_search"))) == 1
+        self.delete_temp_prev_search_table(cursor)
