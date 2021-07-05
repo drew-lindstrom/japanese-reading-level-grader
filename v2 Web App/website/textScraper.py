53d75019e0cf8b46af09707ae111ddecebdd792a
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-07-05 16:30:16 -0500

    Updated to create new Url model and update with data from Url.

--- v2 Web App/website/textScraper.py
+++ v2 Web App/website/textScraper.py
@@ -2,14 +2,27 @@ from bs4 import BeautifulSoup
 import requests
 import sqlite3
 from kanjiDictionary import kanjiDict
+from models import Url
 
 
 class UrlDataObject:
     def __init__(self):
         self.knownKanji = set()
         self.unknownKanji = set()
-        self.totalKanjiCount = {"N5": 0, "N4": 0, "N3": 0, "N2": 0, "N1": 0}
-        self.knownKanjiCount = {"N5": 0, "N4": 0, "N3": 0, "N2": 0, "N1": 0}
+        self.totalKanjiCount = {
+            "JLPT N5": 0,
+            "JLPT N4": 0,
+            "JLPT N3": 0,
+            "JLPT N2": 0,
+            "JLPT N1": 0,
+        }
+        self.knownKanjiCount = {
+            "JLPT N5": 0,
+            "JLPT N4": 0,
+            "JLPT N3": 0,
+            "JLPT N2": 0,
+            "JLPT N1": 0,
+        }
         self.readPercent = 0
 
 
@@ -17,7 +30,8 @@ def getUrlData(url):
     data = getUrlDataSetUp()
     parseTextSetUp(url, data)
     calculateReadPercentage(data)
-    return data
+    dataModel = createNewDataModel(data, url)
+    return dataModel
 
 
 def getUrlDataSetUp():
@@ -62,3 +76,21 @@ def calculateReadPercentage(data):
         knownKanjiTotal += value
 
     return knownKanjiTotal / totalKanjiTotal
+
+
+def createNewDataModel(data, url):
+    model = Url()
+    model.url = url
+    totalN5 = data.totalKanjiCount["JLPT N5"]
+    totalN4 = data.totalKanjiCount["JLPT N4"]
+    totalN3 = data.totalKanjiCount["JLPT N3"]
+    totalN2 = data.totalKanjiCount["JLPT N2"]
+    totalN1 = data.totalKanjiCount["JLPT N1"]
+    knownN5 = data.knownKanjiCount["JLPT N5"]
+    knownN4 = data.knownKanjiCount["JLPT N4"]
+    knownN3 = data.knownKanjiCount["JLPT N3"]
+    knownN2 = data.knownKanjiCount["JLPT N2"]
+    knownN1 = data.knownKanjiCount["JLPT N1"]
+    unknownKanji = str(data.unknownKanji)
+    readPercentage = data.readPercent
+    return model
