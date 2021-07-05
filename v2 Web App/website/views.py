29c7faf75e3cbaa1a4d6857152e4740ccb264a20
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-07-05 16:30:56 -0500

    Minor update to include url model.

--- v2 Web App/website/views.py
+++ v2 Web App/website/views.py
@@ -3,6 +3,7 @@ from flask_login import login_required, current_user
 from .models import Note
 from . import db
 import json
+from textScraper import getUrlData
 
 views = Blueprint("views", __name__)
 
@@ -11,15 +12,15 @@ views = Blueprint("views", __name__)
 @login_required
 def home():
     if request.method == "POST":
-        note = request.form.get("note")
+        url = request.form.get("url")
 
-        if len(note) < 1:
+        if len(url) < 1:
             flash("Note is too short!", category="error")
         else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
+            newUrl = getUrlData(url)
+            db.session.add(newUrl)
             db.session.commit()
-            flash("Note added!", category="success")
+            flash("URL added!", category="success")
 
     return render_template("home.html", user=current_user)
 
