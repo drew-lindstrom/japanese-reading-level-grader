33fd6eb9248b9b6e77b0b0efe94d79ea4e2c61da
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-07-05 15:07:53 -0500

    Updated models.

--- v2 Web App/website/models.py
+++ v2 Web App/website/models.py
@@ -3,9 +3,21 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Note(db.Model):
+class Url(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
+    url = db.Column(db.String(150))
+    N5 = db.Column(db.Integer)
+    N4 = db.Column(db.Integer)
+    N3 = db.Column(db.Integer)
+    N2 = db.Column(db.Integer)
+    N1 = db.Column(db.Integer)
+    uniqueN5 = db.Column(db.Integer)
+    uniqueN4 = db.Column(db.Integer)
+    uniqueN3 = db.Column(db.Integer)
+    uniqueN2 = db.Column(db.Integer)
+    uniqueN1 = db.Column(db.Integer)
+    unknownKanji = db.Column(db.String(2200))
+    readPercentage = db.Column(db.Float)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
@@ -15,4 +27,5 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship("Note")
+    knownKnaji = db.Column(db.String(2200))
+    url = db.relationship("Url")
