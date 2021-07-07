979cb1de7b381cca8ced8d3b204a93c1c93b4b13
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-07-07 13:15:31 -0500

    Updated to use urlId.

--- v2 Web App/website/static/index.js
+++ v2 Web App/website/static/index.js
@@ -1,7 +1,7 @@
-function deleteNote(noteId) {
-    fetch("/delete-note", {
+function deleteUrl(urlId) {
+    fetch("/delete-url", {
         method: "POST",
-        body: JSON.stringify({ noteId: noteId }),
+        body: JSON.stringify({ urlId: urlId }),
     }).then((_res) => {
         window.location.href = "/";
     });
